85386f85ae1baa2826ffa34a1f70c1b0aa4e17a1
Steven Timothy <steven.timothy.cp@gmail.com>
2026-06-29 21:23:51 -0600

    build: upgrade to Angular 22, TypeScript 6, on Node 24

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXhr } from '@angular/common/http';
 import {
   HttpTestingController,
   provideHttpClientTesting,
@@ -13,7 +13,7 @@ describe('ApiService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(withXhr()), provideHttpClientTesting()],
     });
     service = TestBed.inject(ApiService);
     httpMock = TestBed.inject(HttpTestingController);
